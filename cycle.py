befb37e8eb471f5b68739fba1bd3ced632c628a2
sgpro <you@example.com>
2024-08-24 23:13:01 +0500

    initial

---
+++ cycle.py
@@ -0,0 +1,21 @@
+arr1 = [1,2,3,4,5,6]
+arr2 = ["a", "hello", "world"]
+
+a = 1 # integer
+b = "abc" # string
+c = 1.5 # float
+d = True # boolean
+e = None # nothing
+
+arr3 = [1,"222",4.4, True]
+#print(arr2[1])
+
+def SumInArray(arg):
+    arg += " world"
+    return arg
+
+# for index,item in enumerate(arr1):
+#     print(SumInArray(item))
+    
+
+
---
+++ main.py
@@ -0,0 +1,19 @@
+from fpdf import FPDF
+
+arg_string = input("Укажите слово \n")
+font_type = input("\nУкажите стиль ")
+def CreatePdf(arg):
+    pdf=FPDF()
+    pdf.add_page()
+    pdf.set_font('Times',font_type, 24)
+    pdf.cell(40,10, arg)
+    pdf.output('example.pdf','F')
+
+try:
+    CreatePdf(arg_string)
+except UnicodeEncodeError:
+    print("Напишите на английском")
+    arg_string = input("Укажите слово")
+    CreatePdf(arg_string)
+finally:
+    print("Pdf created")
